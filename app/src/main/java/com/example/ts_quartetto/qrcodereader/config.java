53d3010a9e28b903caf549d775b1717ea0ad07df
we333 <wantone321@gmail.com>
2016-10-27 20:26:00 +0900

    add debug flag with Config class

---
+++ app/src/main/java/com/example/ts_quartetto/qrcodereader/config.java
@@ -0,0 +1,13 @@
+package com.example.ts_quartetto.qrcodereader;
+
+/**
+ * Created by we on 2016/10/27.
+ */
+public class Config {
+    private boolean debug = false;
+
+    public boolean isDebug()
+    {
+        return debug;
+    }
+}
---
+++ app/src/main/java/com/example/ts_quartetto/qrcodereader/config_example.java
@@ -0,0 +1,13 @@
+package com.example.ts_quartetto.qrcodereader;
+
+/**
+ * Created by we on 2016/10/27.
+ */
+public class Config_example {
+    private boolean debug = true;
+
+    public boolean getDebugStatus()
+    {
+        return debug;
+    }
+}
